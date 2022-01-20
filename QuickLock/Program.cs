5754cdcfe1adf2928f2db4aea5220f00838fdc6d
Adam <55162814+PanoptesDreams@users.noreply.github.com>
2022-01-21 05:26:08 +1300

    Add project files.

---
+++ QuickLock/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickLock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
